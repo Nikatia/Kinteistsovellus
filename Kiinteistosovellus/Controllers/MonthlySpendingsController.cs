505b73bf960e4ff1afa3c95592ae1e917787e3f3
WizOzzi <90186075+WizOzzi@users.noreply.github.com>
2022-10-31 12:45:31 +0200

    Poistettu tarpeetonta

--- Kiinteistosovellus/Controllers/MonthlySpendingsController.cs
+++ Kiinteistosovellus/Controllers/MonthlySpendingsController.cs
@@ -36,35 +36,7 @@ namespace Kiinteistosovellus.Controllers
             return View(monthlySpendings);
         }
 
-        // GET: MonthlySpendings/Create
-        public ActionResult Create()
-        {
-            ViewBag.LoginID = "1000";
-            ViewBag.ContractorID = new SelectList(db.Contractors, "ContractorID", "Name");
-            
-            ViewBag.SpendingTypeID = new SelectList(db.MonthlySpendingTypes, "SpendingTypeID", "TypeName");
-            return View();
-        }
-
-        // POST: MonthlySpendings/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MonthlySpendingID,DateBegin,DateEnd,SpendingTypeID,AmountOfUnits,PricePerUnit,TransferPayment,FullPrice,ContractorID,LoginID")] MonthlySpendings monthlySpendings)
-        {
-            if (ModelState.IsValid)
-            {
-                db.MonthlySpendings.Add(monthlySpendings);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.LoginID = "1000";
-            ViewBag.ContractorID = new SelectList(db.Contractors, "ContractorID", "Name", monthlySpendings.ContractorID);
-           
-            ViewBag.SpendingTypeID = new SelectList(db.MonthlySpendingTypes, "SpendingTypeID", "TypeName", monthlySpendings.SpendingTypeID);
-            return View(monthlySpendings);
-        }
+      
 
         public ActionResult _CreateModal()
         {
@@ -97,43 +69,7 @@ namespace Kiinteistosovellus.Controllers
             return View(monthlySpendings);
         }
 
-        // GET: MonthlySpendings/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MonthlySpendings monthlySpendings = db.MonthlySpendings.Find(id);
-            if (monthlySpendings == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ContractorID = new SelectList(db.Contractors, "ContractorID", "Name", monthlySpendings.ContractorID);
-            ViewBag.LoginID = new SelectList(db.Logins, "LoginID", "UserName", monthlySpendings.LoginID);
-            ViewBag.SpendingTypeID = new SelectList(db.MonthlySpendingTypes, "SpendingTypeID", "TypeName", monthlySpendings.SpendingTypeID);
-            return View(monthlySpendings);
-            
-        }
-
-        // POST: MonthlySpendings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MonthlySpendingID,DateBegin,DateEnd,SpendingTypeID,AmountOfUnits,PricePerUnit,TransferPayment,FullPrice,ContractorID,LoginID")] MonthlySpendings monthlySpendings)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(monthlySpendings).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ContractorID = new SelectList(db.Contractors, "ContractorID", "Name", monthlySpendings.ContractorID);
-            ViewBag.LoginID = new SelectList(db.Logins, "LoginID", "UserName", monthlySpendings.LoginID);
-            ViewBag.SpendingTypeID = new SelectList(db.MonthlySpendingTypes, "SpendingTypeID", "TypeName", monthlySpendings.SpendingTypeID);
-            return View(monthlySpendings);
-        }
+       
 
         public ActionResult _EditModal(int? id)
         {
@@ -173,31 +109,7 @@ namespace Kiinteistosovellus.Controllers
             return PartialView("_EditModal",monthlySpendings);
         }
 
-        // GET: MonthlySpendings/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MonthlySpendings monthlySpendings = db.MonthlySpendings.Find(id);
-            if (monthlySpendings == null)
-            {
-                return HttpNotFound();
-            }
-            return View(monthlySpendings);
-        }
-
-        // POST: MonthlySpendings/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            MonthlySpendings monthlySpendings = db.MonthlySpendings.Find(id);
-            db.MonthlySpendings.Remove(monthlySpendings);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+       
 
         public ActionResult _DeleteModal(int? id)
         {
