3e06c66e9bb6faac033453216c1759f81208c028
WizOzzi <90186075+WizOzzi@users.noreply.github.com>
2022-11-03 13:50:49 +0200

    script disabled/enable

---
+++ Kiinteistosovellus/Controllers/LoginsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kiinteistosovellus.Models;
+
+namespace Kiinteistosovellus.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private KiinteistoDBEntities db = new KiinteistoDBEntities();
+
+        // GET: Logins
+        public ActionResult Index()
+        {
+            return View(db.Logins.ToList());
+        }
+
+        // GET: Logins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logins);
+        }
+
+        // GET: Logins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LoginID,UserName,UserPassword")] Logins logins)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(logins);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(logins);
+        }
+
+        // GET: Logins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logins);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LoginID,UserName,UserPassword")] Logins logins)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(logins).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(logins);
+        }
+
+        // GET: Logins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logins);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Logins logins = db.Logins.Find(id);
+            db.Logins.Remove(logins);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
