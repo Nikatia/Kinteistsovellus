6711ac7a7e09d42fa43d5331a606d03b236e0292
Monika Fredriksson <moni.fredriksson@gmail.com>
2022-11-04 17:03:33 +0200

    Ensimmäinen urakoitsijoiden perusnäkymä

---
+++ Kiinteistosovellus/ViewModels/PersonsContacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kiinteistosovellus.ViewModels
+{
+    public class PersonsContacts
+    {
+        public int ContractorID { get; set; }
+        public int PersonID { get; set; }
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int LoginID { get; set; }
+    }
+}
