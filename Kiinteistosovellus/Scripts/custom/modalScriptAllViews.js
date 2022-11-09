71e866a58b0d1465cd758b3d2ddb724807dcc480
Miskasa <miska.savenius@gmail.com>
2022-11-09 15:05:16 +0200

    PartialView modaalissa toimii helpotetulla koodilla

--- Kiinteistosovellus/Scripts/custom/modalScriptAllViews.js
+++ Kiinteistosovellus/Scripts/custom/modalScriptAllViews.js
@@ -25,20 +25,6 @@
     });
 };
 
-//function modalGet(controller, actionresult, actionresultId, iframeId, modalId) {
-//    var url = "/" + controller + "/" + actionresult;
-
-//    if (actionresultId != null) {
-//        url += "/?id=" + actionresultId;
-//    }
-
-//    var iframeForModal = $("#" + iframeId);
-//    $.get(url, function (data) {
-//        iframeForModal.replaceWith(data);
-//        $("#" + modalId).modal('show');
-//    });
-//};
-
 function modalGet(controller, actionresult, actionresultId, iframeId, modalId) {
     var url = "/" + controller + "/" + actionresult;
     console.log(actionresultId);
@@ -58,6 +44,70 @@ function modalGet(controller, actionresult, actionresultId, iframeId, modalId) {
         success: function (result) {
             iframeForModal.replaceWith(result);
             $("#" + modalId).modal('show');
+        }
+    });
+};
+
+function partialViewGet(controller, actionresult, realFormDivId) {
+    var url = "/" + controller + "/" + actionresult;
+    var divForPartial = $("#" + realFormDivId);
+
+    $.ajax({
+        url: url, //Haetaan haluttu controller-action
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'get', //pitää spesifioida get, jos halutaan get-tyyppinen toiminto
+        type: "get",
+        success: function (result) {
+            $(divForPartial).html(""); //kaiken varalta tyhjennetään ensin partial view kohta
+            $(divForPartial).html(result); //sitten se täytetään partialViewResultista saadulla partial viewillä
+        }
+    });
+};
+
+function partialViewSubmit(controller, actionresult, realFormDivId, formId, dropdownId) {
+    var divForPartial = $("#" + realFormDivId);
+    var submittedForm = new FormData(document.getElementById(formId));     //Luodaan formdata-tyyppinen muuttuja, joka kerää tiedot halutulta formilta
+    $.ajax({
+        url: "/" + controller + "/" + actionresult,
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'POST', //post pitää spesifioida tässä, jotta saadaan post-tyyppinen toiminto controllerissa
+        type: "POST",
+        data: submittedForm, //aikaisemmin mainittu formdata viedään ajax-pyynnöllä controllerille
+        success: function (result) {
+            //console.log("Result:")
+            //console.log(result);
+            updateDropdownList(controller, "GetList", dropdownId);
+            if (result == "") { //Jos lisäys onnistui, controller palauttaa nullin
+            } else {//tallentaminen ei onnistunut, koska modelstate.isvalid ei ollut true
+                $(divForPartial).html(""); //tyhjennetään vanha modaali (en tiedä, onko pakollista)
+                $(divForPartial).html(result); //luodaan uusi modaali, jossa validate messaget
+            }
+        }
+    });
+};
+
+function updateDropdownList(controller, dropdownId) {
+    var url = "/" + controller + "/GetList";
+    $.ajax({
+        data: {},
+        type: 'POST',
+        cache: false,
+        dataType: 'json',
+        url: url,
+        success: function (result) {
+            $("#" + dropdownId).empty();
+            //$("#OtherSpendingTypeID").append('<option value="">Select One</option>');
+            $.each(result, function (i, item) {
+                $("#" + dropdownId).append('<option value="' + item.Value + '">' +
+                    item.Text + '</option>');
+            });
+        },
+        error: function (ex) {
+            alertify.alert('We face some technical difficulties. Hello World');
         }
     });
 };
