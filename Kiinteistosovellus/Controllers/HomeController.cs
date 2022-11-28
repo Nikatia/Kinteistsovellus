e77da9994b2f777cdb9a478adb721edf1d3e3599
Miskasa <miska.savenius@gmail.com>
2022-11-28 09:05:00 +0200

    Väliaika

--- Kiinteistosovellus/Controllers/HomeController.cs
+++ Kiinteistosovellus/Controllers/HomeController.cs
@@ -16,12 +16,41 @@ namespace Kiinteistosovellus.Controllers
         public ActionResult Index()
         {
             ViewBag.Vuosi = new SelectList(db.MonthlyAndOtherSpendingsByMonth, "Vuosi", "Vuosi");
+            ViewBag.VuosiLine = new SelectList(db.MonthlyAndOtherSpendingsByMonth, "Vuosi", "Vuosi");
             ViewBag.Years = GetYears();
 
             return View();
         }
 
         public ActionResult PieChart(int? year)
+        {
+            string typeList;
+            string priceList;
+            List<ForCategorySortChartWithYearsClass> spendingList = new List<ForCategorySortChartWithYearsClass>();
+
+            var spendingListData = from sld in db.ForCategorySortChartWithYears where sld.SpendingYear == year select sld;
+
+            foreach (ForCategorySortChartWithYears spending in spendingListData)
+            {
+                if (spending.SpendingYear == year)
+                {
+                    ForCategorySortChartWithYearsClass OneRow = new ForCategorySortChartWithYearsClass();
+                    OneRow.Category = spending.Category;
+                    OneRow.Price = (int)spending.Price;
+                    spendingList.Add(OneRow);
+                }
+            }
+
+            typeList = "'" + string.Join("','", spendingList.Select(n => n.Category).ToList()) + "'";
+            priceList = string.Join(",", spendingList.Select(n => n.Price).ToList());
+
+            ViewBag.Category = typeList;
+            ViewBag.Price = priceList;
+
+            return PartialView();
+        }
+
+        public ActionResult _LineChart(int? year)
         {
             string typeList;
             string priceList;
