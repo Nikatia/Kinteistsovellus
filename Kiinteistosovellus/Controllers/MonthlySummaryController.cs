13b04387fdf1f9b0a6507b1084d9e1a1f7f947d7
Monika Fredriksson <moni.fredriksson@gmail.com>
2022-12-12 13:02:49 +0200

    Summary rdy?

--- Kiinteistosovellus/Controllers/MonthlySummaryController.cs
+++ Kiinteistosovellus/Controllers/MonthlySummaryController.cs
@@ -1,6 +1,9 @@
 ﻿using Kiinteistosovellus.Models;
+using Kiinteistosovellus.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,6 +25,36 @@ namespace Kiinteistosovellus.Controllers
         {
             ViewBag.Summary = GetSummary();
             return View(db.SpendingMonths.ToList());
+        }
+
+        public ActionResult _PieChart(int? id)
+        {
+            string typeList;
+            string priceList;
+            List<ForMonthSummary> spendingList = new List<ForMonthSummary>();
+            var monthList = from sm in db.SpendingMonths where sm.Rivi == id select sm;
+            var categoryList = from ms in db.MonthlySummary select ms;
+
+            foreach (SpendingMonths month in monthList)
+            {
+                foreach(MonthlySummary category in categoryList)
+                {
+                    if (month.MonthOfSpending == category.MonthOfSpending && month.YearOfSpending == category.YearOfSpending)
+                    {
+                        ForMonthSummary OneRow = new ForMonthSummary();
+                        OneRow.TypeName = category.TypeName;
+                        OneRow.Kokonaishinta = ((int)category.Kokonaishinta * 100 / (int)month.Summa);
+                        spendingList.Add(OneRow);
+                    }
+                }
+            }
+            typeList = "'" + string.Join("','", spendingList.Select(n => n.TypeName).ToList()) + "'";
+            priceList = string.Join(",", spendingList.Select(n => n.Kokonaishinta).ToList());
+
+            ViewBag.Category = typeList;
+            ViewBag.Price = priceList;
+
+            return PartialView();
         }
     }
 }
